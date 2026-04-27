d1148ec094fb981848322af5b5926bd0d586b971
emanu <emanu@EMANUEL_RAMREZ>
2026-04-27 11:07:44 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Una empresa de monitoreo ambiental necesita registrar las temperaturas de diferentes zonas durante varios días.
+//Solicite al usuario la cantidad de zonas (filas) y la cantidad de días (columnas). 
+//Luego, permita ingresar las temperaturas registradas y muestre la información en forma de tabla organizada.
+
+Console.WriteLine("MONITOREO AMBIENTAL TEMPERATURAS ");
+Console.WriteLine("Ingrese la cantidad de zonas (filas)");
+int zonas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de días (columnas)");
+int dias = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[zonas, dias];
+
+Console.WriteLine("Matriz");
+
+for (int i = 0; i < zonas; i++)
+{
+  for (int j = 0; i < dias; j++) 
+    {
+        Console.WriteLine($"Ingrese la temperatura de la zona {}");
+    }
+}
+
---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Solicitar número de productos");
+int productos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el número de días");
+int dias = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[productos, dias];
+
+for (int i = 0; i < productos; i++)
+{
+    for (int j = 0; j < dias; j++)
+    {
+        Console.WriteLine();
+    }
+}
