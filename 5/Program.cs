345acb49219cdbec6334105158519595295a4f2b
emanu <emanu@EMANUEL_RAMREZ>
2026-05-02 17:11:23 -0600

    Logica Aplicada!

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,6 +1,54 @@
 ﻿
-Console.WriteLine("TIENDA");
-Console.WriteLine("Porfavor Ingrese las dimensiones: fila");
-int fila = int.Parse(Console.ReadLine());
+int f, c, buscar;
+bool encontrado = false;
 
-Console.WriteLine("Ingrese ");
+Console.Write("Filas: ");
+
+while(!int.TryParse(Console.ReadLine(), out f ) || f <= 0)
+{
+    Console.Write("Error: ");
+}
+
+Console.Write("Columnas");
+
+while (int.TryParse(Console.ReadLine(),out c) || c <= 0)
+{
+    Console.WriteLine("Error: ");
+}
+
+int[,] mat = new int[f, c];
+
+for (int i = 0; i < f; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        Console.Write($"Valor [{i},{j}]");
+        while (!int.TryParse(Console.ReadLine(), out mat[i, j]))
+        {
+            Console.Write("Error:");
+        }
+    }
+}
+
+Console.Write("Buscar: ");
+
+while (!int.TryParse(Console.ReadLine(),out buscar))
+{
+    Console.Write("Error: ");
+}
+
+for (int i = 0; i < f; i++)
+{
+    for (i = 0; i < c; i++)
+    {
+        if (mat[i,j] == buscar)
+        {
+            Console.WriteLine($"Encontrado en [{i}, {j}] ");
+            encontrado = true;
+        }
+    }
+}
+if (!encontrado)
+{
+    Console.WriteLine("No existe");
+}
