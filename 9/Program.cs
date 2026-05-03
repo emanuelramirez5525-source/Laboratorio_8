86e4d68e00f1db2c153565c7990c78d8f1e581bd
emanu <emanu@EMANUEL_RAMREZ>
2026-05-02 21:33:33 -0600

    Logica Aplicada!

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,1 +1,54 @@
-﻿  
+﻿int filas, columnas, buscar;
+bool encontrado = false;
+
+Console.Write("Filas: ");
+while (!int.TryParse(Console.ReadLine(), out filas ) || filas <=0)
+{
+    Console.Write("Error: ");
+
+}
+
+Console.Write("Columnas: ");
+while (!int.TryParse(Console.ReadLine(), out columnas) || columnas <= 0)
+{
+    Console.Write("Error: ");
+
+}
+
+int[,] errores = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"Error: [{i},{j}]");
+        while(!int.TryParse(Console.ReadLine(), out errores[i, j]))
+        {
+            Console.Write("Error");
+        }
+    }
+}
+
+Console.Write("Codigo a buscar: ");
+
+while (!int.TryParse(Console.ReadLine(), out buscar))
+{
+    Console.Write("Error:");
+}
+
+for (int i =0; i<filas; i++)
+{
+    for (int j =0; j < columnas; j++)
+    {
+        if (errores[i,j] == buscar)
+        {
+            Console.WriteLine($"Encontrado en [{i},{j}]");
+            encontrado = true;
+        }
+    }
+}
+
+if (!encontrado)
+{
+    Console.WriteLine("No existe");
+}
