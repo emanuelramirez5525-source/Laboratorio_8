034ba49ceaf20ffff1ddbf43ee383460efe4bbbb
emanu <emanu@EMANUEL_RAMREZ>
2026-05-02 18:36:59 -0600

    Logica Aplicada!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int maquinas, turnos, total = 0;
+
+Console.Write("Maquinas: ");
+while (!int.TryParse(Console.ReadLine(), out maquinas) || maquinas <= 0)
+{
+    Console.Write("Error:");
+}
+
+Console.Write("Turnos: ");
+
+while (!int.TryParse(Console.ReadLine(), out turnos) || turnos <= 0)
+{
+    Console.Write("Error:");
+}
+
+int[,] produccion = new int[maquinas, turnos];
+
+for (int i = 0; i <maquinas; j++)
+{
+    for (int j = 0; j < turnos; j++)
+    {
+        Console.Write($"Producción [{i},{j}]");
+        while (!int.TryParse(Console.ReadLine(),out produccion[i, j]))
+        {
+            Console.Write("Error");
+        }
+
+        total += produccion[i, j];
+    }
+}
+
+Console.WriteLine("Producción total: " + total);
